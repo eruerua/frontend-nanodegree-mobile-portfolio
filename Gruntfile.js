bfa80f83aeb0b67684a210b87b291116a720330b
eruera <eruerame@gmail.com>
2017-03-26 15:13:31 +0800

    add grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,52 +5,95 @@
  * Copyright (c) 2014 James Cryer
  * http://www.jamescryer.com
  */
-'use strict'
-
-var ngrok = require('ngrok');
+'use strict';
 
 module.exports = function(grunt) {
-
-  // Load grunt tasks
-  require('load-grunt-tasks')(grunt);
-
   // Grunt configuration
+  require('load-grunt-tasks')(grunt);
   grunt.initConfig({
-    pagespeed: {
-      options: {
-        nokey: true,
-        locale: "en_GB",
-        threshold: 40
-      },
-      local: {
-        options: {
-          strategy: "desktop"
+    uglify: {
+        my_target: {
+          files: {
+            'js/perfmatters.min.js': ['js/perfmatters.js', 'js/!*.min.js']
+          }
+        },
+        my_target2: {
+          files: {
+            'views/js/main.min.js': ['views/js/main.js', 'views/js/!*.min.js']
+          }
         }
       },
-      mobile: {
-        options: {
-          strategy: "mobile"
+      cssmin: {
+        target: {
+          files: [{
+            expand: true,
+            cwd: 'css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'css',
+            ext: '.min.css'
+          }]
+        },
+        target1: {
+          files: [{
+            expand: true,
+            cwd: 'views/css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'views/css',
+            ext: '.min.css'
+          }]
         }
-      }
-    }
-  });
+      },
+      imagemin: {                          // Task
+          dynamic: {
+            options: {                       // Target options
+              optimizationLevel: 3,
+              svgoPlugins: [{ removeViewBox: false }],
+            },                         // Another target
+            files: [{
+              expand: true,                  // Enable dynamic expansion
+              cwd: 'img/',                   // Src matches are relative to this path
+              src: ['*.{png,jpg,gif}'],   // Actual patterns to match
+              dest: 'img/dist/'                  // Destination path prefix
+            }]
+          },
+          dynamic2: {
+            files: [{
+              expand: true,                  // Enable dynamic expansion
+              cwd: 'views/images/',                   // Src matches are relative to this path
+              src: ['*.{png,jpg,gif}'],   // Actual patterns to match
+              dest: 'views/images/dist/'                  // Destination path prefix
+            }]
+          },
 
-  // Register customer task for ngrok
-  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
-    var done = this.async();
-    var port = 9292;
+        },
+        watch: {
+           scripts: {
+            files: ['js/*.js','views/js/*.js'],
+            tasks: 'uglify',
+          options: {
+              spawn: false,
+            },
+           },
+           css: {
+            files: ['css/*.css','views/css/*.css'],
+            tasks: ['cssmin'],
+            options: {
+              spawn: false,
+            },
+           },
+           img: {
+            files: ['img/*.{png,jpg,gif}','views/images/*.{png,jpg,gif}'],
+            tasks: ['imagemin'],
+            options: {
+              spawn: false,
+            },
+           },
 
-    ngrok.connect(port, function(err, url) {
-      if (err !== null) {
-        grunt.fail.fatal(err);
-        return done();
-      }
-      grunt.config.set('pagespeed.options.url', url);
-      grunt.task.run('pagespeed');
-      done();
+
+        },
+
+
     });
-  });
-
   // Register default tasks
-  grunt.registerTask('default', ['psi-ngrok']);
-}
+  grunt.registerTask('default', ['watch']);
+};
