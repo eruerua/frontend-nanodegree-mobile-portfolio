74b1434867dbb3e3738060722dcde74af7297dab
eruera <eruerame@gmail.com>
2017-03-31 15:35:01 +0800

    change the whole layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,12 @@ module.exports = function(grunt) {
     uglify: {
         my_target: {
           files: {
-            'js/perfmatters.min.js': ['js/perfmatters.js', 'js/!*.min.js']
+            'dist/js/perfmatters.min.js': ['src/js/perfmatters.js']
           }
         },
         my_target2: {
           files: {
-            'views/js/main.min.js': ['views/js/main.js', 'views/js/!*.min.js']
+            'dist/views/js/main.min.js': ['src/views/js/main.js']
           }
         }
       },
@@ -27,18 +27,18 @@ module.exports = function(grunt) {
       target: {
         files: [{
           expand: true,
-          cwd: 'css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'css',
+          cwd: 'src/css',
+          src: ['*.css'],
+          dest: 'dist/css',
           ext: '.min.css'
         }]
       },
       target1: {
         files: [{
           expand: true,
-          cwd: 'views/css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'views/css',
+          cwd: 'src/views/css',
+          src: ['*.css'],
+          dest: 'dist/views/css',
           ext: '.min.css'
         }]
       }
@@ -46,43 +46,43 @@ module.exports = function(grunt) {
     imagemin: {                          // Task
         dynamic: {
           options: {                       // Target options
-            optimizationLevel: 5,
+            optimizationLevel: 3,
             svgoPlugins: [{ removeViewBox: false }],
           },                         // Another target
           files: [{
             expand: true,                  // Enable dynamic expansion
-            cwd: 'img/',                   // Src matches are relative to this path
+            cwd: 'src/img/',                   // Src matches are relative to this path
             src: ['*.{png,jpg,gif}'],   // Actual patterns to match
-            dest: 'img/dist/'                  // Destination path prefix
+            dest: 'dist/img/'                  // Destination path prefix
           }]
         },
         dynamic2: {
           files: [{
             expand: true,                  // Enable dynamic expansion
-            cwd: 'views/images/',                   // Src matches are relative to this path
+            cwd: 'src/views/images/',                   // Src matches are relative to this path
             src: ['*.{png,jpg,gif}'],   // Actual patterns to match
-            dest: 'views/images/dist/'                  // Destination path prefix
+            dest: 'dist/views/images/'                  // Destination path prefix
           }]
         },
 
       },
     watch: {
        scripts: {
-        files: ['js/*.js','views/js/*.js'],
+        files: ['src/js/*.js','src/views/js/*.js'],
         tasks: 'uglify',
       options: {
           spawn: false,
         },
        },
        css: {
-        files: ['css/*.css','views/css/*.css'],
+        files: ['src/css/*.css','src/views/css/*.css'],
         tasks: ['cssmin'],
         options: {
           spawn: false,
         },
        },
        img: {
-        files: ['img/*.{png,jpg,gif}','views/images/*.{png,jpg,gif}'],
+        files: ['src/img/*.{png,jpg,gif}','src/views/images/*.{png,jpg,gif}'],
         tasks: ['imagemin'],
         options: {
           spawn: false,
@@ -94,6 +94,5 @@ module.exports = function(grunt) {
     });
   // Register default tasks
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('imgmin', ['imagemin']);
-  grunt.registerTask('ugl', ['uglify']);
+  grunt.registerTask('allwork', ['imagemin','uglify','cssmin']);
 };
