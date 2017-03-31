c63ebe41f268ad9418404430433455038bed64c2
eruera <eruerame@gmail.com>
2017-03-31 14:31:01 +0800

    min js

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,78 +23,77 @@ module.exports = function(grunt) {
           }
         }
       },
-      cssmin: {
-        target: {
-          files: [{
-            expand: true,
-            cwd: 'css',
-            src: ['*.css', '!*.min.css'],
-            dest: 'css',
-            ext: '.min.css'
-          }]
-        },
-        target1: {
-          files: [{
-            expand: true,
-            cwd: 'views/css',
-            src: ['*.css', '!*.min.css'],
-            dest: 'views/css',
-            ext: '.min.css'
-          }]
-        }
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
       },
-      imagemin: {                          // Task
-          dynamic: {
-            options: {                       // Target options
-              optimizationLevel: 5,
-              svgoPlugins: [{ removeViewBox: false }],
-            },                         // Another target
-            files: [{
-              expand: true,                  // Enable dynamic expansion
-              cwd: 'img/',                   // Src matches are relative to this path
-              src: ['*.{png,jpg,gif}'],   // Actual patterns to match
-              dest: 'img/dist/'                  // Destination path prefix
-            }]
-          },
-          dynamic2: {
-            files: [{
-              expand: true,                  // Enable dynamic expansion
-              cwd: 'views/images/',                   // Src matches are relative to this path
-              src: ['*.{png,jpg,gif}'],   // Actual patterns to match
-              dest: 'views/images/dist/'                  // Destination path prefix
-            }]
-          },
-
+      target1: {
+        files: [{
+          expand: true,
+          cwd: 'views/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'views/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    imagemin: {                          // Task
+        dynamic: {
+          options: {                       // Target options
+            optimizationLevel: 5,
+            svgoPlugins: [{ removeViewBox: false }],
+          },                         // Another target
+          files: [{
+            expand: true,                  // Enable dynamic expansion
+            cwd: 'img/',                   // Src matches are relative to this path
+            src: ['*.{png,jpg,gif}'],   // Actual patterns to match
+            dest: 'img/dist/'                  // Destination path prefix
+          }]
         },
-      watch: {
-         scripts: {
-          files: ['js/*.js','views/js/*.js'],
-          tasks: 'uglify',
+        dynamic2: {
+          files: [{
+            expand: true,                  // Enable dynamic expansion
+            cwd: 'views/images/',                   // Src matches are relative to this path
+            src: ['*.{png,jpg,gif}'],   // Actual patterns to match
+            dest: 'views/images/dist/'                  // Destination path prefix
+          }]
+        },
+
+      },
+    watch: {
+       scripts: {
+        files: ['js/*.js','views/js/*.js'],
+        tasks: 'uglify',
+      options: {
+          spawn: false,
+        },
+       },
+       css: {
+        files: ['css/*.css','views/css/*.css'],
+        tasks: ['cssmin'],
         options: {
-            spawn: false,
-          },
-         },
-         css: {
-          files: ['css/*.css','views/css/*.css'],
-          tasks: ['cssmin'],
-          options: {
-            spawn: false,
-          },
-         },
-         img: {
-          files: ['img/*.{png,jpg,gif}','views/images/*.{png,jpg,gif}'],
-          tasks: ['imagemin'],
-          options: {
-            spawn: false,
-          },
-         },
-
-
-      },
+          spawn: false,
+        },
+       },
+       img: {
+        files: ['img/*.{png,jpg,gif}','views/images/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false,
+        },
+       },
+    },
 
 
     });
   // Register default tasks
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('imagmin', ['imagemin']);
+  grunt.registerTask('imgmin', ['imagemin']);
+  grunt.registerTask('ugl', ['uglify']);
 };
